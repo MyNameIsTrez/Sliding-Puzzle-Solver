c2ed468d3d73a654eb35375e7397be4fd1b50aa6
MyNameIsTrez <welfje@gmail.com>
2021-12-11 03:34:02 +0100

    Use a stack instead of recursion

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Sliding-Puzzle-Solver
 
 `python main.py`
-
-94691
-94661
-94661
-94661
-94619
--- main.py
+++ main.py
@@ -1,79 +1,9 @@
+import logging, threading, copy
+
 from enum import Enum, auto
-
-import logging
-
-import sys
-import resource
-
-import threading
-
-
-CHOSEN_PUZZLE = "klotski"
-
-EMPTY_CHARACTER = " "
-
-
-PUZZLES = {
-	"klotski": {
-#		Starting state: A00B10C30D02E12F32G13H23I04J34
-
-#		Move G: A00B10C30D02E12F32G14H23I04J34
-#		Move H: A00B10C30D02E12F32G13H24I04J34
-#		Move I: A00B10C30D02E12F32G13H23I14J34
-#		Move J: A00B10C30D02E12F32G13H23I04J24
-
-		"BOARD_SIZE": {
-			"WIDTH": 4,
-			"HEIGHT": 5
-		},
-
-#		ABBC
-#		ABBC
-#		DEEF
-#		DGHF
-#		I  J
-		"PIECES": {
-			"A": { "pos": { "x": 0, "y": 0 }, "size": { "width": 1, "height": 2 } },
-			"B": { "pos": { "x": 1, "y": 0 }, "size": { "width": 2, "height": 2 } },
-			"C": { "pos": { "x": 3, "y": 0 }, "size": { "width": 1, "height": 2 } },
-			"D": { "pos": { "x": 0, "y": 2 }, "size": { "width": 1, "height": 2 } },
-			"E": { "pos": { "x": 1, "y": 2 }, "size": { "width": 2, "height": 1 } },
-			"F": { "pos": { "x": 3, "y": 2 }, "size": { "width": 1, "height": 2 } },
-			"G": { "pos": { "x": 1, "y": 3 }, "size": { "width": 1, "height": 1 } },
-			"H": { "pos": { "x": 2, "y": 3 }, "size": { "width": 1, "height": 1 } },
-			"I": { "pos": { "x": 0, "y": 4 }, "size": { "width": 1, "height": 1 } },
-			"J": { "pos": { "x": 3, "y": 4 }, "size": { "width": 1, "height": 1 } },
-		}
-	}
-}
-
-
-class Direction(Enum):
-	UP = auto()
-	DOWN = auto()
-	LEFT = auto()
-	RIGHT = auto()
-
-
-PUZZLE = PUZZLES[CHOSEN_PUZZLE]
-
-BOARD_SIZE = PUZZLE["BOARD_SIZE"]
-WIDTH = BOARD_SIZE["WIDTH"]
-HEIGHT = BOARD_SIZE["HEIGHT"]
-
-PIECES = PUZZLE["PIECES"]
 
 
 def main():
-	# This sets the maximum number of bytes used by the stack to 4 GB at 2**32
-	resource.setrlimit(resource.RLIMIT_STACK, (2**32, resource.RLIM_INFINITY))
-
-	# Max recursion depth
-	# Setting this too high will cause the program to silently crash (probably a segfault)
-	# Without the resource.setrlimit() call above 10466 is safe on my Ubuntu laptop,
-	# and 1572 is safe on my Windows 10 PC, according to find_recursionlimit.py
-	sys.setrecursionlimit(int(1e9)) # 1e10 is too large for C
-
 	# filename has to be set or everything will be printed to the terminal
 	# level has to be set for logging.info()
 	# filemode default to "a"
@@ -81,39 +11,42 @@ def main():
 
 	logging.disable() # The code is significantly faster without logging
 
-	state = get_state()
-	states = [ state ]
+	state = get_state(PIECES)
+	STATES.append(state)
 
 	logging.info(f"State number: 1, State: {state}")
 	print(f"\nState number: 1, State: {state}")
 
-	print_board()
+	print_board(PIECES)
 
-	progress_stack = []
-
-	print_progress_stack(progress_stack)
-
-	solve(states, progress_stack)
+	solve()
 
 	logging.info("Done")
 
 	# print("\n\nSOLUTION FOUND!")
 
 	# TODO: Why is this identical to the previously printed board?
-	print_board()
+	# print_board(pieces)
 
-	print(f"\nNumber of states: {len(states)}")
-
-
-def print_progress_stack(progress_stack):
-	threading.Timer(1, print_progress_stack, [progress_stack]).start()
-
-	print(f"progress stack length: {len(progress_stack)}, {progress_stack[:10]}", end="\r", flush=True)
-	# print(f"{progress_stack[-40:]}")
+	print(f"\nNumber of states: {len(STATES)}")
 
 
-def print_board():
-	board = get_board()
+def get_state(pieces):
+	state = ""
+
+	for piece_label, piece in pieces.items():
+		state += piece_label
+
+		pos = piece["pos"]
+		# TODO: Are an X and Y state never going to mix, aka 69 = 6 9? (I don't think so?)
+		state += str(pos["x"])
+		state += str(pos["y"])
+
+	return state
+
+
+def print_board(pieces):
+	board = get_board(pieces)
 
 	for row in board:
 		print(row)
@@ -121,10 +54,10 @@ def print_board():
 	print()
 
 
-def get_board():
+def get_board(pieces):
 	board = [[EMPTY_CHARACTER] * WIDTH for _ in range(HEIGHT)]
 
-	for piece_label, piece in PIECES.items():
+	for piece_label, piece in pieces.items():
 		pos, size = piece["pos"], piece["size"]
 
 		y = pos["y"]
@@ -140,23 +73,53 @@ def get_board():
 	return board
 
 
-def solve(states, progress_stack, depth=0):
-	for piece_label, piece in PIECES.items():
-		progress_stack.append(piece_label)
+# Traverses using depth-first search on the last element on the stack
+def solve():
+	stack = [ { "pieces": copy.deepcopy(PIECES), "path": [] } ]
 
-		# Saves the pos of piece for backtracking
+	timed_print_stack_path(stack)
+
+	while True:
+		node = copy.deepcopy(stack.pop())
+
+		solve_2(node, stack)
+
+		# logging.info("Backtracking due to not having moved and having no more pieces that can be moved")
+
+		if len(stack) == 0:
+			break
+
+		#print("Backtracking due to not having moved and having no more pieces that can be moved")
+
+
+def timed_print_stack_path(stack):
+	threading.Timer(1, timed_print_stack_path, [stack]).start()
+
+	path_string = "".join(stack[-1]["path"][:50])
+
+	print(f"progress stack length: {len(stack)}, {path_string}", end="\r", flush=True)
+	# print(f"{stack[-40:]}")
+
+
+def solve_2(node, stack):
+	depth = len(stack)
+
+	pieces = node["pieces"]
+
+	for piece_label, piece in pieces.items():
+		logging.info(piece_label)
+		logging.info(piece)
+
+		# Save the position of the piece for moving back
 		pos = piece["pos"]
 		x = pos["x"]
 		y = pos["y"]
 
-		logging.info(piece_label)
-		logging.info(piece)
-
 		# For moving a piece up/down/left/right
-		for dir in Direction:
-			logging.info(dir)
+		for direction in Direction:
+			logging.info(direction)
 
-			if is_valid_move(dir, piece, states):
+			if is_valid_move(direction, piece, pieces):
 				logging.info(f"Depth: {depth}")
 				# print(f"Depth: {depth}")
 
@@ -164,37 +127,38 @@ def solve(states, progress_stack, depth=0):
 
 				logging.info("MOVED, RECURSING")
 
-				#print_board()
+				#print_board(pieces)
 
-				solve(states, progress_stack, depth + 1)
+				new_node = copy.deepcopy(node)
+
+				piece_path = f"{piece_label}{DIRECTION_CHARACTERS[direction]} "
+
+				if len(new_node["path"]) < 100: # This check speeds up the program significantly.
+					new_node["path"].append(piece_path)
+
+				stack.append(new_node)
 
 				pos["x"] = x
 				pos["y"] = y
 
-				#print_board()
-
-		progress_stack.pop()
-	
-	logging.info("Backtracking due to not having moved and having no more pieces that can be moved")
-
-	#print("Backtracking due to not having moved and having no more pieces that can be moved")
+				#print_board(pieces)
 
 
-def is_valid_move(dir, piece, states):
+def is_valid_move(direction, piece, pieces):
 	# Saves the position of the piece in case it needs to be moved back
 	pos = piece["pos"]
 	x = pos["x"]
 	y = pos["y"]
 
-	move(dir, pos)
+	move(direction, pos)
 
-	state = get_state()
+	state = get_state(pieces)
 
-	if move_doesnt_cross_puzzle_edge(piece) and no_intersection() and is_new_state(state, states):
-		states.append(state)
+	if move_doesnt_cross_puzzle_edge(piece) and no_intersection(pieces) and is_new_state(state):
+		STATES.append(state)
 
-		logging.info(f"State number: {len(states)}, State: {state}")
-		# print(f"State number: {len(states)}, State: {state}")
+		logging.info(f"State number: {len(STATES)}, State: {state}")
+		# print(f"State number: {len(STATES)}, State: {state}")
 
 		return True
 
@@ -206,29 +170,16 @@ def is_valid_move(dir, piece, states):
 
 
 # TODO: Replace with a lookup/jump table?
-def move(dir, pos):
-	if dir == Direction.UP:
-		pos["y"] += -1
-	elif dir == Direction.DOWN:
-		pos["y"] += 1
-	elif dir == Direction.LEFT:
-		pos["x"] += -1
-	elif dir == Direction.RIGHT:
-		pos["x"] += 1
-
-
-def get_state():
-	state = ""
-
-	for piece_label, piece in PIECES.items():
-		state += piece_label
-
-		pos = piece["pos"]
-		# TODO: Are an X and Y state never going to mix, aka 69 = 6 9? (I don't think so?)
-		state += str(pos["x"])
-		state += str(pos["y"])
-
-	return state
+def move(direction, pos):
+	match direction:
+		case Direction.UP:
+			pos["y"] += -1
+		case Direction.DOWN:
+			pos["y"] += 1
+		case Direction.LEFT:
+			pos["x"] += -1
+		case Direction.RIGHT:
+			pos["x"] += 1
 
 
 # TODO: Replace with a lookup/jump table?
@@ -246,10 +197,10 @@ def move_doesnt_cross_puzzle_edge(piece):
 	return False
 
 
-def no_intersection():
+def no_intersection(pieces):
 	board = [[EMPTY_CHARACTER] * WIDTH for _ in range(HEIGHT)]
 
-	for piece_label, piece in PIECES.items():
+	for piece_label, piece in pieces.items():
 		pos, size = piece["pos"], piece["size"]
 
 		y = pos["y"]
@@ -268,11 +219,72 @@ def no_intersection():
 	return True
 
 
-# TODO:
-# Efficiently store which states have already been visited to avoid infinite loops
-def is_new_state(state, states):
-	return state not in states
+def is_new_state(state):
+	return state not in STATES
 
 
 if __name__ == "__main__":
+	CHOSEN_PUZZLE = "klotski"
+
+	EMPTY_CHARACTER = " "
+
+
+	PUZZLES = {
+		"klotski": {
+	#		Starting state: A00B10C30D02E12F32G13H23I04J34
+
+	#		Move G: A00B10C30D02E12F32G14H23I04J34
+	#		Move H: A00B10C30D02E12F32G13H24I04J34
+	#		Move I: A00B10C30D02E12F32G13H23I14J34
+	#		Move J: A00B10C30D02E12F32G13H23I04J24
+
+			"BOARD_SIZE": {
+				"WIDTH": 4,
+				"HEIGHT": 5
+			},
+
+	#		ABBC
+	#		ABBC
+	#		DEEF
+	#		DGHF
+	#		I  J
+			"PIECES": {
+				"A": { "pos": { "x": 0, "y": 0 }, "size": { "width": 1, "height": 2 } },
+				"B": { "pos": { "x": 1, "y": 0 }, "size": { "width": 2, "height": 2 } },
+				"C": { "pos": { "x": 3, "y": 0 }, "size": { "width": 1, "height": 2 } },
+				"D": { "pos": { "x": 0, "y": 2 }, "size": { "width": 1, "height": 2 } },
+				"E": { "pos": { "x": 1, "y": 2 }, "size": { "width": 2, "height": 1 } },
+				"F": { "pos": { "x": 3, "y": 2 }, "size": { "width": 1, "height": 2 } },
+				"G": { "pos": { "x": 1, "y": 3 }, "size": { "width": 1, "height": 1 } },
+				"H": { "pos": { "x": 2, "y": 3 }, "size": { "width": 1, "height": 1 } },
+				"I": { "pos": { "x": 0, "y": 4 }, "size": { "width": 1, "height": 1 } },
+				"J": { "pos": { "x": 3, "y": 4 }, "size": { "width": 1, "height": 1 } }
+			}
+		}
+	}
+
+	class Direction(Enum):
+		UP = auto()
+		DOWN = auto()
+		LEFT = auto()
+		RIGHT = auto()
+
+	DIRECTION_CHARACTERS = {
+		Direction.UP: "^",
+		Direction.DOWN: "v",
+		Direction.LEFT: "<",
+		Direction.RIGHT: ">",
+	}
+
+
+	PUZZLE = PUZZLES[CHOSEN_PUZZLE]
+
+	BOARD_SIZE = PUZZLE["BOARD_SIZE"]
+	WIDTH = BOARD_SIZE["WIDTH"]
+	HEIGHT = BOARD_SIZE["HEIGHT"]
+
+	PIECES = PUZZLE["PIECES"]
+
+	STATES = []
+
 	main()

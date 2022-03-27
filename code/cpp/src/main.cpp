bfb6d871c3af2bf23d490f76da0392b243b26e53
MyNameIsTrez <welfje@gmail.com>
2022-03-27 05:14:17 +0200

    Managed to read the puzzle JSON file in C++.

--- code/cpp/src/main.cpp
+++ code/cpp/src/main.cpp
@@ -1,41 +1,45 @@
 // #include "main.hpp"
 
-// #include <iostream> // std::cout, std::endl
-// #include <stdlib.h> // EXIT_SUCCESS
+////////
 
-// int main(void)
-// {
-// 	std::cout << "Hello World!" << std::endl;
-// 	return (EXIT_SUCCESS);
-// }
-
-#include <iostream>
-#include "json.hpp"
-
+#include "json.hpp" // json
 using json = nlohmann::json;
 
-int main()
+////////
+
+#include <iostream> // std::cout, std::endl
+#include <fstream>	// ifstream
+
+////////
+
+std::filesystem::path get_puzzle_path_from_exe_path(std::filesystem::path exe_path)
 {
-	std::string s = R"(
-    {
-        // update in 2006: removed Pluto
-        "planets": ["Mercury", "Venus", "Earth", "Mars",
-                    "Jupiter", "Uranus", "Neptune" /*, "Pluto" */]
-    }
-    )";
+	std::filesystem::path exe_dir_path = exe_path.remove_filename();
+	std::filesystem::path puzzle_path = exe_dir_path / "puzzles" / "klotski.jsonc";
+	return puzzle_path;
+}
 
-	try
-	{
-		json j = json::parse(s);
-	}
-	catch (json::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	json j = json::parse(s,
+json get_puzzle_json(std::filesystem::path puzzle_path)
+{
+	std::ifstream f(puzzle_path);
+	json j = json::parse(f,
 						 /* callback */ nullptr,
 						 /* allow exceptions */ true,
 						 /* ignore_comments */ true);
-	std::cout << j.dump(2) << '\n';
+	return j;
+}
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	std::filesystem::path exe_path = argv[0];
+
+	std::filesystem::path puzzle_path = get_puzzle_path_from_exe_path(exe_path);
+
+	json j = get_puzzle_json(puzzle_path);
+
+	std::cout << j.dump(4) << std::endl;
+
+	return (EXIT_SUCCESS);
 }
